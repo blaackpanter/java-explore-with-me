b956c5b11b4c12004e3cdf7e1f7146423478dbb8
elena.pranova <elena.pranova@corp.mail.ru>
2024-06-17 12:48:06 +0300

    add max

--- ewm-server/src/main/java/ru/practicum/ewmserver/event/dto/EventFullDto.java
+++ ewm-server/src/main/java/ru/practicum/ewmserver/event/dto/EventFullDto.java
@@ -11,7 +11,6 @@ import ru.practicum.ewmserver.event.model.Location;
 import ru.practicum.ewmserver.user.dto.UserShortDto;
 import ru.practicum.statdto.dto.Constants;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
